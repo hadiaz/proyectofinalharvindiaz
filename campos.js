223d6c3441afd878461fe46cc841358be497e2f1
hadiaz <56024123+hadiaz@users.noreply.github.com>
2019-10-02 18:20:07 -0600

    Add files via upload

---
+++ campos.js
@@ -0,0 +1,10 @@
+var campos ={
+"type": "FeatureCollection",
+"name": "campos",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "OBJECTID": 28, "Codigo": 8, "Descripc": "Campo de Balónpie", "Tipo": 8 }, "geometry": { "type": "Point", "coordinates": [ -88.784750, 14.770563 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": 122, "Codigo": 8, "Descripc": "Campo de Balónpie", "Tipo": 8 }, "geometry": { "type": "Point", "coordinates": [ -88.784686, 14.774599 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": 319, "Codigo": 8, "Descripc": "Campo de Balónpie", "Tipo": 8 }, "geometry": { "type": "Point", "coordinates": [ -88.783271, 14.780966 ] } },
+]
+}
