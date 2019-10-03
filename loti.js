72b4f07ee084067e30f36c23b990699c1b8d1f79
hadiaz <56024123+hadiaz@users.noreply.github.com>
2019-10-02 18:33:16 -0600

    Add files via upload

---
+++ loti.js
@@ -0,0 +1,16 @@
+type: "FeatureCollection",
+
+crs: { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+features: [
+{ "type": "Feature", "properties": { "D000000_": 3211, "D000000_ID": 1882, "GEOCODIGO": 
+{ "type": "Feature", "properties": { "D000000_": 3229, "D000000_ID": 1889, "GEOCODIGO": 
+{ "type": "Feature", "properties": { "D000000_": 3258, "D000000_ID": 1894, "GEOCODIGO": 
+{ "type": "Feature", "properties": { "D000000_": 3276, "D000000_ID": 1899, "GEOCODIGO": 
+{ "type": "Feature", "properties": { "D000000_": 3288, "D000000_ID": 1903, "GEOCODIGO": 
+{ "type": "Feature", "properties": { "D000000_": 3289, "D000000_ID": 1904, "GEOCODIGO":
+{ "type": "Feature", "properties": { "D000000_": 3322, "D000000_ID": 1912, "GEOCODIGO": 
+{ "type": "Feature", "properties": { "D000000_": 3349, "D000000_ID": 1922, "GEOCODIGO": 
+{ "type": "Feature", "properties": { "D000000_": 3351, "D000000_ID": 1923, "GEOCODIGO": 
+{ "type": "Feature", "properties": { "D000000_": 3369, "D000000_ID": 1930, "GEOCODIGO":
+]
+}
